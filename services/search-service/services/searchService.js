eed146af05e08dc7e597adf8468f9de94e5c1f6f
DavidKoljnrekaj <koljnrekajdavid@gmail.com>
2024-11-24 22:43:37 +0100

    searchService, listing to shared

---
+++ services/search-service/services/searchService.js
@@ -0,0 +1,27 @@
+const Listing = require('../../../shared/constants/models/listingModel'); 
+
+// Get short listings with pagination and optional filters
+exports.getShortListings = async (filters = {}, page = 1, limit = 10) => {
+  const skip = (page - 1) * limit;
+
+  const query = {};
+
+  //add filters laterrr
+  if (filters.city) query['location.city'] = filters.city;
+  if (filters.type) query['propertyDetails.type'] = filters.type;
+  if (filters.minPrice) query['price.monthlyRent'] = { $gte: filters.minPrice };
+  if (filters.maxPrice) {
+    query['price.monthlyRent'] = {
+      ...query['price.monthlyRent'],
+      $lte: filters.maxPrice,
+    };
+  }
+
+  const listings = await Listing.find(query, 'title price.monthlyRent images location.city propertyDetails.type')
+    .skip(skip)
+    .limit(limit);
+
+  const total = await Listing.countDocuments(query);
+
+  return { listings, total };
+};
