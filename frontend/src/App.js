2f55df14ababa96cdbb931876b62cf440e312922
DavidKoljnrekaj <koljnrekajdavid@gmail.com>
2024-12-11 11:04:52 +0100

    Login/Register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/shared/Navbar/Navbar';
 import Home from './pages/home/Home';
-import Login from './components/login/Login';
-import Register from './components/register/Register';
+import Register from './components/auth/Register';
 import Listing from './pages/Listing/Listing';
 import ListingsPage from './pages/ListingsPage/ListingsPage'
 
@@ -13,7 +12,6 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/listing/:id" element={<Listing />} />
         <Route path="/listings" element={<ListingsPage />} />
