41421b9d4fec0264a0f31d44cde890d801d458a1
Lovrevar <lovrento@gmail.com>
2024-12-12 08:03:25 +0100

    User service favorites

--- services/user-service/controllers/userController.js
+++ services/user-service/controllers/userController.js
@@ -19,3 +19,31 @@ exports.getUsers = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
+
+exports.addFavorite = async (req, res) => {
+  try {
+    const favorites = await userService.addFavorite(res.body.listingId, req.user.id)
+    res.status(200).json(favorites);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.removeFavorite = async (req, res) => {
+  try {
+    const favorites = await userService.removeFavorite(res.body.listingId, req.user.id)
+    res.status(200).json(favorites);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.getFavorites = async (req, res) => {
+  try {
+    const favorites = await userService.getFavorites(req.user.id)
+    res.status(200).json(favorites);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
