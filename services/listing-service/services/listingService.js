c1f71c0f6047435723e7719c99bb718b81973068
DavidKoljnrekaj <koljnrekajdavid@gmail.com>
2024-12-17 19:24:34 +0100

    deployment fix

--- services/listing-service/services/listingService.js
+++ services/listing-service/services/listingService.js
@@ -3,7 +3,7 @@ const { Storage } = require('@google-cloud/storage');
 const path = require('path');
 
 // Path to your service account key file
-const credentials = JSON.parse(process.env.GOOGLE_CLOUD_KEY);
+const credentials = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
 
 
